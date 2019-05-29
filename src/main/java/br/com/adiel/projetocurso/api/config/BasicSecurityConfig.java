c2f145d9b55d16baa403a7793a86348b7f8fe437
adiel <adielrr@gmail.com>
2019-05-28 20:50:58 -0400

    ok

--- src/main/java/br/com/adiel/projetocurso/api/config/BasicSecurityConfig.java
+++ src/main/java/br/com/adiel/projetocurso/api/config/BasicSecurityConfig.java
@@ -2,41 +2,42 @@ package br.com.adiel.projetocurso.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-
-@Profile("oauth-security")
-@Configuration
+@Profile("basic-security")
 @EnableWebSecurity
-@EnableAuthorizationServer
-@EnableResourceServer
-public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		System.out.println("01");
 		auth.inMemoryAuthentication()
 			.withUser("admin").password("$2a$10$irrav3MNgYxbPgkEw05j0.0G/dpcQsZ1VWijPZY8kMRfrjSZwSnKq").roles("ROLE");
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic()
+			.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.csrf().disable();
+	}
+	
 }
-
--- src/main/java/br/com/adiel/projetocurso/api/config/ResourceServerConfig.java
+++ src/main/java/br/com/adiel/projetocurso/api/config/ResourceServerConfig.java
@@ -2,12 +2,14 @@ package br.com.adiel.projetocurso.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+@Profile("oauth-security")
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
